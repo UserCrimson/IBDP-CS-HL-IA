d46690f3fbc5ce9cf1a1aead886707d08c784934
UserCrimson <151024152+UserCrimson@users.noreply.github.com>
2023-11-24 12:28:06 +0100

    Update SignUP.swift

--- SignUP.swift
+++ SignUP.swift
@@ -11,8 +11,10 @@ struct Signup: View {
   
         @State private var Username = ""
         @State private var Password = ""
+        @State private var confirmpassword = ""
         @State private var wrongUsername = 0
         @State private var wrongPassword = 0
+        @State private var confirmPassword = 0
         @State private var showingLogInScreen = false
         
         var body: some View {
@@ -29,7 +31,6 @@ struct Signup: View {
                 .foregroundColor(.white)
             
             
-            
             VStack{
             
                 Image("Icon")
@@ -38,7 +39,7 @@ struct Signup: View {
                     .frame(width: 100, height: 120)
                     .padding(.vertical,1)
                 
-                Text("SignUp")
+                Text("Register")
                     .font(.largeTitle)
                     .bold()
                     .padding()
@@ -58,6 +59,36 @@ struct Signup: View {
                     .cornerRadius(10)
                     .border(.black,width: 2)
                     .border(.red,width: CGFloat (wrongPassword))
+                
+                TextField("Confirm Password", text: $confirmpassword)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.white.opacity(0.75))
+                    .cornerRadius(10)
+                    .border(.black, width: 2)
+                    .border(.red,width: CGFloat (confirmPassword))
+                
+          
+        
+                
+                
+                
+                
+                
+             
+                
+                
+                Button("Register"){
+                    
+                    
+                        
+                    
+                    
+                }
+                .foregroundColor(.white)
+                .frame(width: 300, height: 50)
+                .background(Color.blue)
+                .cornerRadius(10)
                 
             }
         }
