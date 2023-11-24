df91437e65e45b24065c1524353a1581721b83c2
UserCrimson <151024152+UserCrimson@users.noreply.github.com>
2023-11-24 01:59:22 +0100

    Update LogInPage.swift

--- LogInPage.swift
+++ LogInPage.swift
@@ -25,8 +25,6 @@ struct ContentView: View {
     var body: some View{
         NavigationView{
             
-            
-            
             ZStack{
                 Color
                     .white.opacity(0.10)
@@ -58,6 +56,7 @@ struct ContentView: View {
                         .border(.red,width: CGFloat (wrongPassword))
                     
                     Button("Login"){
+                        authenticateUser(Username: Username, Password: Password)
                         
                     }
                     .foregroundColor(.white)
@@ -65,16 +64,51 @@ struct ContentView: View {
                     .background(Color.blue)
                     .cornerRadius(10)
                     
+                    
+                    
+                    // Temp
                     NavigationLink(destination: Text("you have been logged in @\(Username)"), isActive: $showingLogInScreen) {
                         EmptyView()
+                    // Temp will be changed to a different page which will be the homepage after it has been developed
+                    
                         
+                        
+                        
+                        Button("Sign up"){
+                            
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
                     }
                 }
-                .navigationBarHidden(true)
+            }
+            .navigationBarHidden(true)
+        }
+    }
+        
+        func authenticateUser(Username: String, Password: String ){
+            if Username.lowercased() == "tester" {
+                wrongUsername = 0
+                if Password.lowercased() == "pass"{
+                    wrongPassword = 0
+                    showingLogInScreen = true
+        
+                } else {
+                    wrongPassword = 2
+                }
+                    
+            }else {
+                wrongUsername = 2
+                    
+                
+                
             }
         }
     }
-}
+
+
   
     
     
