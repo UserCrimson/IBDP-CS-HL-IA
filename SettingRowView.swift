c7576374118880d74a9c8067d7b0c0746e762d16
UserCrimson <151024152+UserCrimson@users.noreply.github.com>
2023-12-15 14:16:40 +0100

    Update SettingRowView.swift

--- SettingRowView.swift
+++ SettingRowView.swift
@@ -1,1 +1,37 @@
+//
+//  SettingRowView.swift
+//  IA
+//
+//  Created by CHRISTIE, Russell on 15.12.23.
+//
 
+import SwiftUI
+
+struct SettingRowView: View {
+    let imagename: String
+    let title:String
+    let tintColor: Color
+    
+    // This is to save time, so instead of writing the same code i will just call this
+    
+    var body: some View {
+        HStack(spacing: 14) {
+            Image(systemName: imagename)
+                .imageScale(.small)
+                .font(.title)
+                .foregroundColor(tintColor)
+            
+            Text(title)
+                .font(.subheadline)
+                .foregroundColor(.black)
+            
+        }
+        
+    }
+}
+
+struct SettingRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingRowView(imagename: "gear", title: "Version", tintColor: Color(.systemGray3))
+    }
+}
