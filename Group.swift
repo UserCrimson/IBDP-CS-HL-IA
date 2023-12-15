0d3c53dccbdf3ef3ab9809966da613a6bc851946
UserCrimson <151024152+UserCrimson@users.noreply.github.com>
2023-12-15 09:39:42 +0100

    Update Group.swift

--- Group.swift
+++ Group.swift
@@ -1,1 +1,11 @@
+import Foundation
+import RealmSwift
+
+final class Group: Object, ObjectKeyIdentifiable {
+    
+    @Persisted(primaryKey: true) var _id:ObjectId
+    
+    @Persisted var Items = RealmSwift.List<Item>()
+    
+}
 
