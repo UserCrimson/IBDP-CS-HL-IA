2a2c5e2bc980aa37875cd87bb34d8f2752de74a0
UserCrimson <151024152+UserCrimson@users.noreply.github.com>
2024-01-21 17:29:30 +0100

    Update UserModel.swift

--- UserModel.swift
+++ UserModel.swift
@@ -1,1 +1,32 @@
+//
+//  User Model.swift
+//  IA
+//
+//  Created by CHRISTIE, Russell on 19.12.23.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    let fullname: String
+    let email: String
+    
+    var initails: String{
+        let formatter = PersonNameComponentsFormatter()
+        if let components = formatter.personNameComponents(from: fullname){
+            
+            formatter.style = .abbreviated
+            return formatter.string(from: components)
+        }
+        
+        return ""
+    }
+}
+
+extension User {
+    static var MOCK_USER = User(id: NSUUID().uuidString, fullname: "Russell Christie", email: "RussChris@outlook.com" )
+    
+    
+}
 
