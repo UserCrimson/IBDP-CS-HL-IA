07fbb38bc70ee25b24480e146662286613690810
UserCrimson <151024152+UserCrimson@users.noreply.github.com>
2024-01-21 17:26:53 +0100

    Update LogInPage.swift

--- LogInPage.swift
+++ LogInPage.swift
@@ -17,11 +17,9 @@
 import SwiftUI
 struct Login: View {
     // Declaring Variable names for future use
-    @State private var Username = ""
+    @State private var Email = ""
     @State private var Password = ""
-    @State private var wrongUsername = 0
-    @State private var wrongPassword = 0
-    @State private var showingLogInScreen = false
+    @EnvironmentObject var viewModel: AuthViewModel
     
     var body: some View{
         NavigationView{
@@ -42,84 +40,77 @@ struct Login: View {
                 
                 // Creating the buttons and text fields that are going to be used an an input and buttons for login and sign up
                 VStack{
+                    Spacer()
+                    
                     Text("Login")
                         .font(.largeTitle)
                         .bold()
                         .padding()
-                    TextField("Username", text: $Username)
+                    
+
+                    TextField("Email", text: $Email)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background(Color.white.opacity(0.75))
                         .cornerRadius(10)
-                        .border(.red,width: CGFloat (wrongUsername))
-                    
                     SecureField("Password", text: $Password)
                         .padding()
                         .frame(width: 300, height: 50)
                         .background(Color.white.opacity(0.75))
                         .cornerRadius(10)
-                        .border(.red,width: CGFloat (wrongPassword))
+                      
                     
-                    Button("Login"){
-                        authenticateUser(Username: Username, Password: Password)
-                        
-                    }
-                    .foregroundColor(.white)
-                    .frame(width: 300, height: 50)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    
-                    
-                    // Current Work - RC 06 19
-                    NavigationLink{
-                        Signup()
+                    Button{
+                        Task {
+                            try await viewModel.signIn(withEmail: Email, password: Password)
+                        }
                     }label:{
                         HStack(spacing: 3){
-                            Text("Dont have an account")
-                            Text("Sign up")
-                                .fontWeight(.bold)
+                            Text("Log In")
+                                .fontWeight(.semibold)
+                            Image(systemName: "arrow.right")
                         }
-                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                        .frame(width: 300, height: 50)
                     }
-            
-            
-
-                    // Temp
-                    NavigationLink(destination: Text("you have been logged in @\(Username)"), isActive: $showingLogInScreen) {
-                        EmptyView()
-                    // Temp will be changed to a different page which will be the homepage after it has been developed
+                    .background(Color(.systemBlue))
+                    .cornerRadius(10)
+                    .padding(.top, 24)
                     
-                        // need to use a stackview for the sign up page then create a local database for the username and password - todo
-            
+                    Spacer()
+                        
+                        // Current Work - RC 06 19
+                    
+                    NavigationLink{
+                        Signup()
+                            .navigationBarBackButtonHidden(true)
+                    }label:{
+                        HStack(spacing: 3){
+                            Text("Dont have an ccount?")
+                            Text("Sign up")
+                                .fontWeight(.semibold)
+                        }
+                        .font(.system(size: 14))
+                    }
+                        
+                        
+                            
+        
+                            
+                            
+                            
+                        }
                     }
                 }
-            }
-            .navigationBarHidden(true)
-        }
-    }
-        
-        func authenticateUser(Username: String, Password: String ){
-            if Username.lowercased() == "tester" {
-                wrongUsername = 0
-                if Password.lowercased() == "pass"{
-                    wrongPassword = 0
-                    showingLogInScreen = true
-        
-                } else {
-                    wrongPassword = 2
-                }
-                    
-            }else {
-                wrongUsername = 2
-                    
-                
-                
+                .navigationBarHidden(true)
             }
         }
-    }
 
 
-    struct ContentView_Previews: PreviewProvider {
+
+
+
+    struct Login_Previews: PreviewProvider {
         static var previews: some View {
             Login()
         }
