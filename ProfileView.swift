3be7f263fe52354919910badc877fcc850bc1474
UserCrimson <151024152+UserCrimson@users.noreply.github.com>
2024-01-24 10:22:23 +0100

    Update ProfileView.swift

--- ProfileView.swift
+++ ProfileView.swift
@@ -8,75 +8,80 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    
     var body: some View {
-        List{
-            // User info View
-            Section{
-                HStack{
-                    Text(User.MOCK_USER.initails)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(width: 72, height: 72)
-                        .background(Color(.systemGray3))
-                        .clipShape(Circle())
-                    
-                    VStack(alignment: .leading, spacing: 4){
-                        Text(User.MOCK_USER.fullname)
-                            .font(.subheadline)
+        if let user = viewModel.currentUser{
+            List{
+                // User info View
+                Section{
+                    HStack{
+                        Text(user.initails)
+                            .font(.title)
                             .fontWeight(.semibold)
-                            .padding(.top, 4)
+                            .foregroundColor(.white)
+                            .frame(width: 72, height: 72)
+                            .background(Color(.systemGray3))
+                            .clipShape(Circle())
                         
-                        Text(User.MOCK_USER.email)
-                            .font(.footnote)
-                            .foregroundColor(.gray)
+                        VStack(alignment: .leading, spacing: 4){
+                            Text(user.fullname)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                            
+                            Text(user.email)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                            
+                        }
                         
                     }
                     
                 }
                 
-            }
-            
-            Section("General"){
-                HStack{
-                    SettingRowView(
-                        imagename: "gear",
-                        title: "Version",
-                        tintColor: Color(.systemGray))
+                Section("General"){
+                    HStack{
+                        SettingRowView(
+                            imagename: "gear",
+                            title: "Version",
+                            tintColor: Color(.systemGray))
+                        
+                        Spacer()
+                        
+                        Text("1.0.0")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                         
+                    }
+                }
+                
+                Section("Account") {
+                    Button{
+                        viewModel.signOut()
+                    } label:{
+                        SettingRowView(imagename: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
+                    }
                     
-                    Spacer()
                     
-                    Text("1.0.0")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                     
+                    Button{
+                        print("Delete Account")
+                    } label:{
+                        SettingRowView(imagename: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
                 }
-            }
-            
-            Section("Account") {
-                Button{
-                    print("Sign Out")
-                } label:{
-                    SettingRowView(imagename: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
-                }
-                
-                
-                Button{
-                    print("Delete Account")
-                } label:{
-                    SettingRowView(imagename: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
-                }
-                
-                
-                
-                
-                
-                
             }
         }
     }
-}
 
+        }
+       
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
